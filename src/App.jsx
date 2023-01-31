8022469aca86d566b52ce5243c1ef1fc73ed38d6
evren <sukruevrengokduman@gmail.com>
2023-01-31 22:34:31 +0300

    added newsletter component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Analytics from "./components/Analytics";
-
+import Newsletter from "./components/Newsletter";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }
--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,10 +4,21 @@ const Analytics = () => {
   return (
     <div className="w-full bg-white py-16 px-4">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-        <img src={Laptop} />
-        <div>
-          <p>DATA ANALYTICS DASHBOARD</p>
-          <h1>Manage Data Analytics Centrally</h1>
+        <img className="w-[500px] mx-auto my-4" src={Laptop} />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+          <h1 className="md:text-4xl sm:text-3xl font-bold py-2">
+            Manage Data Analytics Centrally
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+            recusandae dicta, possimus, fugiat ipsa minus quisquam molestiae
+            atque distinctio est rerum ipsum aliquid fuga fugit doloremque nulla
+            porro voluptatem? Nihil.
+          </p>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 md:mx-0 py-3 mx-auto">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
