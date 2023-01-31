52c6e0caabb92a654237536051407da672787a33
evren <sukruevrengokduman@gmail.com>
2023-01-31 13:08:44 +0300

    colllapse bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] height-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 "
+            ? "fixed left-0 top-0 w-[60%] height-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden "
             : "fixed left-[-100%]"
         }>
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-7">REACT.</h1>
