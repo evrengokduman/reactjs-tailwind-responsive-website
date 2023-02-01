452c0c8bffc0b0ee8d0074bd16d833987ba45fb5
evren <sukruevrengokduman@gmail.com>
2023-02-01 14:25:16 +0300

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="max-w-[1240] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+    <div className="max-w-[1240] mx-auto py-16 px-16 grid lg:grid-cols-3 gap-8 text-gray-300">
       <div>
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
         <p className="py-4">
